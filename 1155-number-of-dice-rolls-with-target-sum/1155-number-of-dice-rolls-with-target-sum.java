84c655d66a75a8c05bcd8b3df1332830c34c8785
Sohini Bhattacharya <64258756+sohini3062@users.noreply.github.com>
2022-05-08 22:36:18 +0530

    Time: 91 ms (22.46%), Space: 44.4 MB (23.41%) - LeetHub

---
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.java
@@ -0,0 +1,29 @@
+class Solution {
+    int MOD=1000000007;
+    public int numRollsToTarget(int n, int k, int target) {
+        long[][] dp=new long[n+1][target+1];
+        /*  dp[i][j]= total number of ways first i dice to make a sum of j
+             dp[0][0]=1
+            Initialization : All others are 0
+            Transition: dp[i][j]=dp[i][j]+ dp[i-1][j-dice] where dice=1 to k and j-dice>=0
+            Final: dp[n][target] */
+        dp[0][0]=1;
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=target;j++)
+            {
+                for(int dice=1;dice<=k;dice++)
+                {
+                    if(j-dice>=0)
+                    {
+                        dp[i][j]+=dp[i-1][j-dice];
+                        dp[i][j]=dp[i][j]%MOD;
+                    }
+                        
+                }
+            }
+        }
+        return (int)(dp[n][target]);
+        
+    }
+}
