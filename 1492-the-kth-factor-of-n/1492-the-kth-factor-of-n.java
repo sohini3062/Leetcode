236c9936b5e223d660f42784fa0279dab8283c35
Sohini Bhattacharya <64258756+sohini3062@users.noreply.github.com>
2022-06-07 08:51:53 +0530

    Time: 1 ms (83.26%), Space: 41.6 MB (17.20%) - LeetHub

--- 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.java
+++ 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.java
@@ -1,17 +1,13 @@
+import java.util.*;
 class Solution {
     public int kthFactor(int n, int k) {
-        PriorityQueue<Integer> pq=new PriorityQueue<>(Collections.reverseOrder());
-        for(int i=1;i<=n;i++)
-        {
-            if(n%i==0)
-                pq.add(i);
-            if(pq.size()>k)
-                pq.remove();
+        for (int factor = 1 ; factor <= n ; factor++) {
+            if ( n % factor == 0) {
+                k--;
+                if (k == 0) return factor;
+            }
         }
-        
-        if(pq.size()<k)
-            return -1;
-        return pq.peek();
+        return -1;
         
     }
 }
